6c58b6b2c306f19e0c37ecafd18f207562fcdcc1
DEBJYOTIRUMAN <debjyotiruman@gmail.com>
2024-04-04 04:39:56 +0530

    Change CSS

--- Client/components/messages/Call/Container.js
+++ Client/components/messages/Call/Container.js
@@ -176,7 +176,7 @@ const Container = ({
           />
         </div>
       )}
-      <div className="my-5 relative sm:h-screen/2" id="remote-video">
+      <div className="relative sm:h-screen/2" id="remote-video">
         <div className="absolute bottom-5 right-5 sm:bottom-1 sm:right-1" id="local-video"></div>
       </div>
       <div className="h-16 w-16 bg-red-600 flex items-center justify-center rounded-full sm:h-12 sm:w-12">
