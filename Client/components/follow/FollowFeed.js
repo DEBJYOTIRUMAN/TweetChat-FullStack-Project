4b1c51d49adaafaa5b27d0c2ec2c349eadf90ded
DEBJYOTIRUMAN <debjyotiruman@gmail.com>
2024-04-04 08:09:45 +0530

    Minor Bug Fixed

--- Client/components/follow/FollowFeed.js
+++ Client/components/follow/FollowFeed.js
@@ -3,7 +3,7 @@ import ConnectUserCard from "../connect/ConnectUserCard";
 
 const style = {
   wrapper: `border-r border-l border-[#38444d] w-[40rem] md:w-[30rem] sm:max-w-[20rem] xs:!max-w-[17rem]`,
-  header: `sticky top-0 bg-[#15202b] z-10 p-4 flex items-center`,
+  header: `top-0 bg-[#15202b] z-10 p-4 flex items-center`,
   headerTitle: `text-xl font-bold mx-4`,
   section: `bg-[#192734] my-6 rounded-xl overflow-hidden`,
   nav: `flex justify-around mt-4 mb-2 font-semibold text-[#8899a6]`,
